a44344528d49cd55f2d6e054b2748dac173da897
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-08 15:53:33 +0700

    Add Navbar and UserItem components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
 import { Component } from "react";
 import "./App.css";
-import Age from "./components/Age";
-import Name from "./components/Name";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 
 class App extends Component {
-  state = {
-    name: "Le Dinh Nghia Hung",
-    age: 100000,
-    address: "658 Ngo Quyen",
-    showName: false,
-    showLoading: true,
-  };
-
-  increaseAge = () => {
-    this.setState({
-      age: this.state.age + 1,
-    });
-  };
-
-  decreaseAge = () => {
-    this.setState({
-      age: this.state.age - 1,
-    });
-  };
-
-  resetAge = () => {
-    this.setState({
-      age: 0,
-    });
-  };
-
-  switchName = () => {
-    this.setState({
-      showName: !this.state.showName,
-    });
-  };
-
-  switchLoading = () => {
-    this.setState({
-      showLoading: !this.state.showLoading,
-    });
-  };
-
   render() {
     return (
-      <div className="App">
-        <Name name={this.state.name} />
-        <Age age={this.state.age} />
-        <p>My Address: {this.state.address}</p>
-        <button onClick={this.increaseAge}>Increase my age</button>
-        <button onClick={this.decreaseAge}>Decrease my age</button>
-        <button onClick={this.resetAge}>Clear my Age</button>
-        <button onClick={this.switchName}>Switch the name</button>
-        {this.state.showLoading ? <h4>Loading</h4> : <h4>No loading ...</h4>}
-        <button onClick={this.switchLoading}>Switch the loading</button>
+      <div>
+        <Navbar />
+        <div className="container">
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+        </div>
       </div>
     );
   }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="navbar bg-primary">Hello TCD0301-React</div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
