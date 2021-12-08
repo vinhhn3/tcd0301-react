38dbc701629da138aa71ccbf609aa903e447cc79
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-08 16:49:02 +0700

    Use axios to send http request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import axios from "axios";
 import { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    usersData: [],
+  };
+  componentDidMount() {
+    console.log("App.js is mounted ...");
+    axios.get("https://api.github.com/users").then((response) => {
+      console.log(response.data);
+      this.setState({
+        usersData: response.data,
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users usersData={this.state.usersData} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,44 +2,14 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    usersData: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "pjhyett",
-        id: 4,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "pjhyett",
-        id: 5,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-    ],
-  };
+  componentDidMount() {
+    console.log("Users component is mounted ...");
+  }
+
   render() {
     return (
       <div style={userStyle}>
-        {this.state.usersData.map((user) => (
+        {this.props.usersData.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
