0882be9072e985a84c9f5628abc0a8925644b415
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-11 09:40:30 +0700

    Setup and Use React Router v5.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Search from "./components/users/Search";
 import Users from "./components/users/Users";
 
@@ -31,20 +35,34 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar title={this.state.title} />
-        <div className="container">
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            usersData={this.state.usersData}
-          />
-          <Users
-            showLoading={this.state.showLoading}
-            usersData={this.state.usersData}
-          />
+      <Router>
+        <div>
+          <Navbar title={this.state.title} />
+          <div className="container">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      usersData={this.state.usersData}
+                    />
+                    <Users
+                      showLoading={this.state.showLoading}
+                      usersData={this.state.usersData}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route path="" component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
