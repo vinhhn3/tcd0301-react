30aa7f5278190494a0f4591ae68e73f59b37c35e
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-15 15:03:55 +0700

    More refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import GithubState from "./context/github/githubState";
 
@@ -8,9 +7,7 @@ const App = () => {
   return (
     <GithubState>
       <Router>
-        <div>
-          <Home />
-        </div>
+        <Home />
       </Router>
     </GithubState>
   );
